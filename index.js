67ee884daaaddcd7fa7e4f3c1622385af8960420
virajdomadia <53313911+virajdomadia@users.noreply.github.com>
2023-05-08 22:05:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var Image1 = document.querySelectorAll("img")[0];
+Image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var Image2 = document.querySelectorAll("img")[1];
+Image2.setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML = "It Is a Draw"
+}
